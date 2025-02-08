1af82c2ae149f646548df4945692ead8c938c485
grithwikreddy <grith2004@gmail.com>
2025-02-09 00:27:03 +0530

    H2 database

---
+++ src/main/java/com/crud/H2DataLearning/H2DataLearningApplication.java
@@ -0,0 +1,13 @@
+package com.crud.H2DataLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DataLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DataLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/H2DataLearning/H2DataLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.H2DataLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DataLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
